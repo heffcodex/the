8cecc05871f90f5ef0fde42caac948bcbe26e90d
Vladislav K <vladislav@heffcodex.pro>
2025-04-22 20:31:12 +0200

    chore: multiple improvements

--- tdep/bakedin/bun/postgres.go
+++ tdep/bakedin/bun/postgres.go
@@ -1,40 +1,34 @@
-package tdep
+package tdep_bun
 
 import (
 	"context"
 	"database/sql"
 	"fmt"
-	"time"
 
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
 	"github.com/uptrace/bun/driver/pgdriver"
 	"github.com/uptrace/bun/extra/bundebug"
 	"go.uber.org/zap"
+
+	"github.com/heffcodex/the/tdep"
 )
 
-type BunConfig struct {
-	DSN            string `mapstructure:"dsn" json:"dsn" yaml:"dsn"`
-	MaxConnections int    `mapstructure:"maxConnections" json:"maxConnections" yaml:"maxConnections"`
-	MaxIdleTime    int    `mapstructure:"maxIdleTime" json:"maxIdleTime" yaml:"maxIdleTime"`
+var _ IDB = (*bun.DB)(nil)
+
+type IDB interface {
+	bun.IDB
+	PingContext(ctx context.Context) error
 }
 
-func (c *BunConfig) MaxIdleTimeSeconds() time.Duration {
-	if c.MaxIdleTime < 1 {
-		return 0
-	}
-
-	return time.Duration(c.MaxIdleTime) * time.Second
-}
-
-func NewBunPostgres(
-	cfg BunConfig,
+func NewPostgres[C IDB](
+	cfg Config,
 	onTuneConnector func(conn *pgdriver.Connector),
 	onTuneSQLDB func(db *sql.DB),
 	onTuneBunDB func(db *bun.DB),
-	options ...Option,
-) *D[*bun.DB] {
-	resolve := func(o OptSet) (*bun.DB, error) {
+	options ...tdep.Option,
+) *tdep.D[C] {
+	resolve := func(o tdep.OptSet) (C, error) {
 		connOpts := []pgdriver.Option{
 			pgdriver.WithApplicationName(o.Name()),
 			pgdriver.WithDSN(cfg.DSN),
@@ -72,10 +66,10 @@ func NewBunPostgres(
 			),
 		)
 
-		return bunDB, nil
+		return any(bunDB).(C), nil //nolint:errcheck,revive // should never panic
 	}
 
-	return New(resolve, options...).WithHealthCheck(func(ctx context.Context, d *D[*bun.DB]) error {
+	return tdep.New(resolve, options...).WithHealthCheck(func(ctx context.Context, d *tdep.D[C]) error {
 		instance, err := d.Get()
 		if err != nil {
 			return fmt.Errorf("get: %w", err)
